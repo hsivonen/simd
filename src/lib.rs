8cd55ee2562e75273bcda6a7311384b6b2a3d0a6
Steven Fackler <sfackler@gmail.com>
2017-02-01 21:07:01 -0800

    Fix serde support

--- src/lib.rs
+++ src/lib.rs
@@ -13,29 +13,29 @@
 //!
 //! This is **not** currently the `simd` crate on crates.io.
 
-#![cfg_attr(feature = "serde", feature(plugin, custom_derive))]
-#![cfg_attr(feature = "serde", plugin(serde_macros))]
-
 #![feature(cfg_target_feature, repr_simd, platform_intrinsics, const_fn)]
 #![allow(non_camel_case_types)]
 
-#[cfg(feature = "serde")]
+#[cfg(feature = "with-serde")]
 extern crate serde;
+#[cfg(feature = "with-serde")]
+#[macro_use]
+extern crate serde_derive;
 
 /// Boolean type for 8-bit integers.
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct bool8i(i8);
 /// Boolean type for 16-bit integers.
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct bool16i(i16);
 /// Boolean type for 32-bit integers.
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct bool32i(i32);
 /// Boolean type for 32-bit floats.
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct bool32f(i32);
 
@@ -74,90 +74,90 @@ pub unsafe trait Simd {
 
 /// A SIMD vector of 4 `u32`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u32x4(u32, u32, u32, u32);
 /// A SIMD vector of 4 `i32`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i32x4(i32, i32, i32, i32);
 /// A SIMD vector of 4 `f32`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct f32x4(f32, f32, f32, f32);
 /// A SIMD boolean vector for length-4 vectors of 32-bit integers.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool32ix4(i32, i32, i32, i32);
 /// A SIMD boolean vector for length-4 vectors of 32-bit floats.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool32fx4(i32, i32, i32, i32);
 
 #[allow(dead_code)]
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 struct u32x2(u32, u32);
 #[allow(dead_code)]
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 struct i32x2(i32, i32);
 #[allow(dead_code)]
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 struct f32x2(f32, f32);
 #[allow(dead_code)]
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 struct bool32ix2(i32, i32);
 #[allow(dead_code)]
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 struct bool32fx2(i32, i32);
 
 /// A SIMD vector of 8 `u16`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u16x8(u16, u16, u16, u16,
                  u16, u16, u16, u16);
 /// A SIMD vector of 8 `i16`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i16x8(i16, i16, i16, i16,
                  i16, i16, i16, i16);
 /// A SIMD boolean vector for length-8 vectors of 16-bit integers.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool16ix8(i16, i16, i16, i16,
                      i16, i16, i16, i16);
 
 /// A SIMD vector of 16 `u8`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u8x16(u8, u8, u8, u8, u8, u8, u8, u8,
                  u8, u8, u8, u8, u8, u8, u8, u8);
 /// A SIMD vector of 16 `i8`s.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i8x16(i8, i8, i8, i8, i8, i8, i8, i8,
                  i8, i8, i8, i8, i8, i8, i8, i8);
 /// A SIMD boolean vector for length-16 vectors of 8-bit integers.
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool8ix16(i8, i8, i8, i8, i8, i8, i8, i8,
                      i8, i8, i8, i8, i8, i8, i8, i8);
--- src/v256.rs
+++ src/v256.rs
@@ -19,83 +19,83 @@ use super::sixty_four::*;
 use super::x86::avx::common;
 
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u64x4(u64, u64, u64, u64);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i64x4(i64, i64, i64, i64);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct f64x4(f64, f64, f64, f64);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool64ix4(i64, i64, i64, i64);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool64fx4(i64, i64, i64, i64);
 
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u32x8(u32, u32, u32, u32,
                  u32, u32, u32, u32);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i32x8(i32, i32, i32, i32,
                  i32, i32, i32, i32);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derfve(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct f32x8(f32, f32, f32, f32,
                  f32, f32, f32, f32);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool32ix8(i32, i32, i32, i32,
                      i32, i32, i32, i32);#[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool32fx8(i32, i32, i32, i32,
                      i32, i32, i32, i32);
 
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u16x16(u16, u16, u16, u16, u16, u16, u16, u16,
                   u16, u16, u16, u16, u16, u16, u16, u16);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i16x16(i16, i16, i16, i16, i16, i16, i16, i16,
                   i16, i16, i16, i16, i16, i16, i16, i16);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool16ix16(i16, i16, i16, i16, i16, i16, i16, i16,
                       i16, i16, i16, i16, i16, i16, i16, i16);
 
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct u8x32(u8, u8, u8, u8, u8, u8, u8, u8,
                  u8, u8, u8, u8, u8, u8, u8, u8,
                  u8, u8, u8, u8, u8, u8, u8, u8,
                  u8, u8, u8, u8, u8, u8, u8, u8);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct i8x32(i8, i8, i8, i8, i8, i8, i8, i8,
                  i8, i8, i8, i8, i8, i8, i8, i8,
                  i8, i8, i8, i8, i8, i8, i8, i8,
                  i8, i8, i8, i8, i8, i8, i8, i8);
 #[repr(simd)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy)]
 pub struct bool8ix32(i8, i8, i8, i8, i8, i8, i8, i8,
                      i8, i8, i8, i8, i8, i8, i8, i8,
